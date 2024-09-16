ab2b0e09e3898b1d5f42c54b6a0304bab657076c
tooscco <aladejuyigbetosin7@gmail.com>
2024-09-16 14:20:41 +0100

    working

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AdminComponent } from './admin/admin.component';
+import { StaffComponent } from './staff/staff.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path: '', component: HomeComponent
+    },
+    {
+        path: 'Admin', component: AdminComponent
+    },
+    {
+        path: 'Staff', component: StaffComponent
+    },
+  ];
